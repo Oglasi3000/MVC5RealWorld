782174b46e6c415b4a9bd8bd245edc510b9b80b5
hrvoje.protulipac <hrvoje.protulipac@RS-1OT-45OAR191>
2018-06-07 15:53:52 +0200

    Add project files.

---
+++ MVC5RealWorld/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using MVC5RealWorld.Security;
+using System.Web.Mvc;
+namespace MVC5RealWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+
+    }
+}
